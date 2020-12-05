0156d8e4703f7ae4515ca1b1eb587ad119430408
EnriqueGonzalezMartinez <adriangzz2001@gmail.com>
2020-12-04 21:31:26 -0600

    Se agrega funcion en powershell

---
+++ Hash_values.ps1
@@ -0,0 +1,15 @@
+﻿param(
+    [Parameter(Mandatory)] [string] $path
+)
+$HashFile = 'Hash('+(Get-Date -Format "yyyy-MM-dd")+' '+(Get-Date -Format "HH-mm-ss")+').txt'
+if (Test-Path -Path $path){
+    if ((Get-Item $path) -is [System.IO.DirectoryInfo]){
+        Get-ChildItem $path | Get-FileHash | Select-Object -Property Hash, Path | Format-Table -HideTableHeaders | Out-File $HashFile -Encoding ascii -ErrorAction SilentlyContinue
+        Write-Host "Hash file was created successfully"
+    }else{
+        Get-FileHash -Algorithm SHA256 -Path $path | Out-File $HashFile -Encoding ascii
+        Write-Host "Hash file was created successfully"
+    }
+}else{
+    Write-Host "The path not exist"
+}
---
+++ hash_files.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+def hashValuer(path):
+    cwd = os.getcwd().replace('\\','/')
+    if (cwd != path):
+        command = 'powershell -ExecutionPolicy ByPass -File Hash_values.ps1 -path ' + path
+        subprocess.run(command)
+    else:
+        print('It is not possible to get the hash of the same directory from which the script is run.')
